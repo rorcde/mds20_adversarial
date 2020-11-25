b1c188c190271061c713ac733757e3636b3176fd
Daniil Moskovskiy <50806739+etomoscow@users.noreply.github.com>
2020-11-25 13:22:28 +0300

    added docstrings

--- models/classifiers.py
+++ models/classifiers.py
@@ -4,14 +4,33 @@ import torch.nn.functional as F
 
 
 class TextCNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
-                 dropout, pad_idx):
+    '''
+    Convolutional Neural Network for text classification based on [1].
+    Credits for implementation: [2]
+    Inputs:
+        vocab_size - size of vocabulary of dataset used
+        emb_dim - embedding space dimension
+        n_filters - number of filters
+        filter_sizes - sizes of convolution filters
+        output_dim - dimension of the output (number of classes in our case)
+        dropout - drop out probability
+        pad_idx - padding index for sequences
+    [1]: Yoon Kim. Convolutional neural networks for sentence classification. (2014)
+    [2]: https://github.com/etomoscow/DL-in-NLP/blob/master/hw3/task4_sentiment_cnn.ipynb
+    '''
+
+    def __init__(self, vocab_size, emb_dim, n_filters, filter_sizes, output_dim, dropout, pad_idx):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
-        self.convs = nn.ModuleList([nn.Conv1d(in_channels=embedding_dim, out_channels=n_filters, kernel_size=fs) for fs in filter_sizes])
-        self.fc = nn.Linear(len(filter_sizes)*n_filters, output_dim)
+        self.embedding = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
+        self.convs = nn.ModuleList([
+            nn.Conv1d(in_channels=emb_dim,
+                      out_channels=n_filters,
+                      kernel_size=fs)
+            for fs in filter_sizes
+        ])
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
         self.dropout = nn.Dropout(dropout)
-        
+
     def forward(self, text):
         embs = self.embedding(text)
         embs = embs.permute(0, 2, 1)
@@ -21,45 +40,33 @@ class TextCNN(nn.Module):
         final = self.fc(cat)
         return final
 
-class GRU_Text(nn.Module):
-    def __init__(self,
-                 vocab_size,
-                 emb_dim,
-                 hidden_dim,
-                 out_dim,
-                 dropout,
-                 pad_idx
-                 ):
+
+class TextGRU(nn.Module):
+    '''
+    Recurrent Neural Network based on Gated Rectified Unit for text classification [1].
+    Credits for implementation: [2]
+    Inputs:
+        vocab_size - size of vocabulary of dataset used
+        emb_dim - embedding space dimension
+        hidden_dim - dimension of hidden state of RNN
+        output_dim - dimension of the output (number of classes in our case)
+        dropout - drop out probability
+        pad_idx - padding index for sequences
+    [1]: Empirical evaluation of gated recurrent neural networks on sequence modeling, 2014.
+    [2]: https://github.com/etomoscow/DL-in-NLP/blob/master/hw3/task3_sentiment_rnn.ipynb
+    '''
+
+    def __init__(self, vocab_size, emb_dim, hidden_dim, out_dim, dropout, pad_idx):
         super().__init__()
-        self.embedding = nn.Embedding(
-            vocab_size,
-            emb_dim,
-            padding_idx=pad_idx
-            )
-        self.rnn = nn.GRU(
-            emb_dim,
-            hidden_dim,
-            num_layers=1,
-            bidirectional=True
-            )
+        self.embedding = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
+        self.rnn = nn.GRU(emb_dim, hidden_dim, num_layers=1, bidirectional=True, dropout=dropout)
         self.fc = nn.Linear(hidden_dim * 2, out_dim)
         self.dropout = nn.Dropout(dropout)
-        
+
     def forward(self, text, text_lengths):
-        #text = [sent len, batch size]
         embedded = self.dropout(self.embedding(text))
-        #embedded = [sent len, batch size, emb dim]
-        #pack sequence
-        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths.detach().cpu())
-        packed_output, hidden = self.rnn(packed_embedded)
-        #unpack sequence
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths)
+        packed_output, (hidden, cell) = self.rnn(packed_embedded)
         output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
-        #output = [sent len, batch size, hid dim * num directions]
-        #output over padding tokens are zero tensors
-        #hidden = [num layers * num directions, batch size, hid dim]
-        #cell = [num layers * num directions, batch size, hid dim]
-        #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
-        #and apply dropout
-        hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
-        #hidden = [batch size, hid dim * num directions]
+        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
         return self.fc(hidden)
