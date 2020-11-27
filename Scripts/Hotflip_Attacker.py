e41ae50323fe3eafb42b80ab568f81322f40090f
Daniil Moskovskiy <50806739+etomoscow@users.noreply.github.com>
2020-11-27 23:06:28 +0300

    Delete Hotflip_Attacker.py

--- Scripts/Hotflip_Attacker.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-import numpy as np
-import matplotlib.pyplot as plt
-
