da1c47c28bafae3fa3ac2d5e821a961e64e7d300
Daniil Moskovskiy <50806739+etomoscow@users.noreply.github.com>
2020-12-14 12:47:59 +0300

    minor fixes

--- utils/prepare_dataset.py
+++ utils/prepare_dataset.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer
 from datasets import load_dataset
 import torch
 
-def prepare_trec_dataset():
+def prepare_trec_dataset(batch_size):
 	tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 	train_dataset = load_dataset('trec', split='train')
 	train_dataset = train_dataset.map(
@@ -12,6 +12,6 @@ def prepare_trec_dataset():
 	test_dataset = test_dataset.map(
 		lambda e: tokenizer(e['text'], truncation=True, padding='max_length'),
 		batched=True)
-	trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=64)
-	testloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
+	trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, drop_last=True)
+	testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, drop_last=True)
 	return trainloader, testloader, tokenizer	
