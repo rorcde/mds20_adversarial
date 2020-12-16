201fed96f08d5b71908f12e3f6a5421a52a05840
Margo <shark.margaret@gmail.com>
2020-12-16 23:42:03 +0300

    add forward_on_embeddings for gumbel one hot vecors

--- models/deep_lev.py
+++ models/deep_lev.py
@@ -1,27 +1,14 @@
-import math
 import torch
 import torch.nn as nn
 
+
 class Deep_lev(torch.nn.Module):
-    '''
-    This `Model` implements a framework for fast approximate similarity search as described in [Convolutional Embedding for Edit Distance](https://arxiv.org/abs/2001.11692).
-    The `DeepLevenshtein` takes two sequences (texts) `x` and `y` as inputs outputs an approximated edit-distance score between `x` and `y`.
 
-    # Parameters
-    sequance_a.shape = [seq_len, batch_size]
-    sequance_b.shape = [seq_len, batch_size]
-    approx_distance.shape = [batch_size, 1]
-    distance - initial distance between two tokenized sequences
-
-    '''
-
-    def __init__(self, vocab_size, embedding_dim, hidden_dim) :
+    def __init__(self, vocab_size=30522, embedding_dim=128, hidden_dim=128) :
         super().__init__()
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.encoder = nn.LSTM(embedding_dim, hidden_dim, batch_first=False)
-
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.encoder = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
         self.linear = nn.Linear(hidden_dim * 3, 1)
-        self._loss = nn.MSELoss()
 
 
     def encode_sequence(self, sequence):
@@ -29,7 +16,25 @@ class Deep_lev(torch.nn.Module):
         out, (ht, ct) = self.encoder(embedded_sequence)
         return ht[-1]
 
-    def forward(self, sequence_a, sequence_b, distance = None):
+    def get_embeddings(self, onehots_a, onehots_b):
+        embs_a = torch.stack([torch.matmul(v, self.embeddings.weight) for v in onehots_a])
+        
+        return embs_a
+
+    def forward_on_embeddings(self, sequence_a, sequence_b):
+        embs_a = self.get_embeddings(sequence_a, sequence_b)
+        _, (embedded_sequence_a, _) = self.encoder(embs_a)
+        embedded_sequence_a= embedded_sequence_a[-1]
+        
+        embedded_sequence_b = self.encode_sequence(sequence_b)
+        diff = torch.abs(embedded_sequence_a - embedded_sequence_b)
+        representation = torch.cat([embedded_sequence_a, embedded_sequence_b, diff], dim=-1)
+        approx_distance = self.linear(representation)
+
+        return approx_distance
+
+
+    def forward(self, sequence_a, sequence_b):
         embedded_sequence_a = self.encode_sequence(sequence_a)
         embedded_sequence_b = self.encode_sequence(sequence_b)
         diff = torch.abs(embedded_sequence_a - embedded_sequence_b)
@@ -38,4 +43,3 @@ class Deep_lev(torch.nn.Module):
         approx_distance = self.linear(representation)
 
         return approx_distance
-
